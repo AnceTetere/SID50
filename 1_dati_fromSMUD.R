c02da7e526c47465d7969bfb71d14a2380c104f2
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-09-16 10:41:59 +0300

    Update 1_dati_fromSMUD.R

--- 1_dati_fromSMUD.R
+++ 1_dati_fromSMUD.R
@@ -1,10 +1,13 @@
-# Dati no SMUD: Izmanto 4-tā ceturkšņa failu 
-
-tab_name <- paste0("SID50", substr(year, 3, 4), "c4")
+#Izmanto 4-tā ceturkšņa failu 
+tab_name <- paste0("DS_struktura_DB", substr(year, 3, 4), "c4")
 wb <- loadWorkbook(paste0("../SID50/", year, "/", year, "Q4/SMUD/", tab_name, ".xlsx"))
-# wb <- loadWorkbook(paste0("../SID50/", year, "_(pārrēķināts 2024. gadā)/", year, "Q4/SMUD/", tab_name, ".xlsx"))
 ws <- readWorksheet(wb, sheet = 1)
-rm(wb)
+rm(wb, tab_name)
 
 ws <- ws[ ,c("DAT", "VSN", "nT", "NOZ2", "G5")]
 
+t216 <- ws[ws$nT == "216", ]
+colnames(t216)[colnames(t216) == "G5"] <- "t216"
+
+rownames(ws) <- NULL
+rownames(t216) <- NULL
