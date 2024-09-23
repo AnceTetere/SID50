c67244b8917a12d4ea5de83169091d8441696da9
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-09-23 12:17:21 +0300

    Update 0_fails_forDB.R

--- 0_fails_forDB.R
+++ 0_fails_forDB.R
@@ -1,49 +1,45 @@
 #install.packages("tidyverse")
+#install.packages("XLConnect")
 library(tidyverse)
+library(XLConnect)
 
-base_path <- "../SID50/"
+base_path <- "DB/SID50c/"
 year <- readline(prompt = "Ievadīt gadu: ") 
 path <- paste0(base_path, year, "/")
 if(!dir.exists(path)) {dir.create(path)}
 
-#No iepriekšējā gada faila izstrādā šablonu SID50
+source(paste0(base_path, "R_kods/fnc_rename_dataframes.R"))
+
+#No iepriekšējā gada faila izstrādā šablonu SID50c
 #setwd(paste0(base_path, as.numeric(year) - 1, "\\DB_fails"))
-SID50 <- read.csv2("../SID50/DB_fails/SID50.csv")
-SID50_ailes <- readRDS("../SID50/R_kods/SID50_ailes.rds")
+SID50c <- read.csv2("DB/SID50c/DB_fails/SID50c.csv")
+SID50c_ailes <- readRDS("../SID50c/R_kods/SID50c_ailes.rds")
 
 #Sākuma šablons un saglabāt
-  y <- SID50
-  y <- y[, SID50_ailes]
+  y <- SID50c
+  y <- y[, SID50c_ailes]
   y$THSD_EUR[is.na(y$THSD_EUR)] <- ""
   y$THSD_EUR_X[is.na(y$THSD_EUR_X)] <- ""
   rownames(y) <- NULL
 
-initial_SID50 <- y
-rm(y)
+  initial_SID50c <- y
+  rm(y)
 
-#setwd(paste0(path, "intermediate_tables\\fails_forDB"))
-#save(initial_SID50, file = "initial_SID50.RData")
-#rm(initial_SID50)
+#Gada šablons
+nameDB <- paste0("additional_SID50c_", year)
+assign(nameDB, SID50c[SID50c$TIME == as.numeric(year) - 1, ])
 
-#Gada šablons un saglabāt
-nameDB <- paste0("additional_SID50_", year)
-assign(nameDB, SID50[SID50$TIME == as.numeric(year) - 1, ])
-  
   x <- get(nameDB)
-  x$TIME <-paste0(year)
+  x$TIME <- year
   rownames(x) <- NULL
   x$THSD_EUR <- ""
+  x$THSD_EUR_X <- ""
   
   x$S <- factor(x$S) # 3 levels
   x$I <- factor(x$I) # 16 levels
   assign(nameDB, x)
 
 #---------------TEST
-final_rows_SID50 <- nrow(SID50) + nrow(x)
-#saveRDS(final_rows_SID50, file = "final_rows_SID50.RDS")
-rm(x, SID50)
-
-#setwd(paste0(path, "intermediate_tables/fails_forDB"))
-#save(list = nameDB, file = paste0(nameDB, ".RData"))
-#rm(list = nameDB)
-#rm(nameDB)
+x <- x[!(is.na(x$I) & is.na(x$S) & is.na(x$N)), ]
+final_rows_SID50c <- nrow(initial_SID50c) + nrow(x)
+rm(x, SID50c)
